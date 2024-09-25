4d6da1fa3a2ff33ef501b4584fb565df8e40fcc9
code with edison <edsnkvn@gmail.com>
2024-09-25 16:06:06 +0200

    some changes

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const userRoutes = require('./routes/userRoutes');
 const app = express();
 
-// Middleware to parse JSON
+// Middleware to parse JSON responses
 app.use(express.json());
 
 // Mounting user routes at /api/users
